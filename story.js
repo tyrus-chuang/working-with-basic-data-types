8f4fa3ddaf0192e30fbaf36890fd71b8a4910049
tyrus-chuang <109128588+tyrus-chuang@users.noreply.github.com>
2022-07-11 22:15:07 -0700

    commit

---
+++ story.js
@@ -0,0 +1,21 @@
+let hello = "Hi person!";
+let nameQ = "What is your name?";
+let myName = "John";
+let theirName = "Billy";
+
+let theirAge = 25;
+let myAge = 30;
+let currentYear = 2010;
+
+let favoriteFoods = ["pizza", "burgers", "sushi", "steak"];
+
+let ateFavoriteFood = false;
+
+console.log("???: " + hello + nameQ + "My name is " + myName);
+console.log("Me: I am " + myName + ". I am " + myAge + " years old.");
+console.log(theirName + ": I am " + theirAge + " years old.");
+console.log(theirName + ": You just woke up from a very long nap. The year is " + currentYear + ". Your actual age is 59.");
+console.log(theirName + ": Anyways... Let's talk about our favorite foods!");
+console.log(theirName + ": I like" + favoriteFoods[0] + ", " + favoriteFoods[1] + ", " + favoriteFoods[2] + ", " + favoriteFoods[3]);
+console.log(theirName + ": " + (theirName == false ? "I haven't eaten any of them today." : "I have eaten one of them today."));
+console.log(myName + ": That is very cool.");
